2677a444d470c3bb25da1dac076dc3df191b6a1d
Richard Marles <marleswebinfo@gmail.com>
2024-02-19 10:27:16 +0100

    adding heroku.com to allowed_hosts settings

--- easyiq/settings.py
+++ easyiq/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j(ull!ke!lnybiy)qk!58#gx^g=emrn$322ex93go0)b5od+_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['heroku.com']
 
 
 # Application definition
