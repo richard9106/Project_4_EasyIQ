a8494ad62eb28818c12bd63bc4c6fe97611820e2
Richard Marles <marleswebinfo@gmail.com>
2024-02-19 10:38:59 +0100

    changing heroku for herokuapp in allowed hosts

--- easyiq/settings.py
+++ easyiq/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j(ull!ke!lnybiy)qk!58#gx^g=emrn$322ex93go0)b5od+_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.heroku.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
